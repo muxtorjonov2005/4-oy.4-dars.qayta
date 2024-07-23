46f9cb5211f6d546e1900639b94853b3a65cbf88
muxtorjonov2005 <muxtorjonoveldiyor@gmail.com>
2024-07-23 12:24:02 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,143 @@
+// 1-masala
+// let sum =0
+// for(let i = 11; i <= 100; i += 2){
+//   sum += i
+//   console.log(sum);
+// }
+
+
+// 2-masala
+// let res = 1
+// let num = +prompt('son kirit')
+// for(let i = 1; i <= num; i++){
+//   res *= i
+//   console.log(res);
+// }
+
+
+// 3-masala
+
+
+// 4-masala
+// let res
+// let sum = 0
+// for(let i = 1; i <= 20; i++){
+//   res = i ** 2
+//   sum += res
+//   console.log(sum);
+// }
+
+
+// 5-masala 
+// for(let i = 1; i <= 5; i++){
+//   let num = +prompt('son kirit')
+// }
+
+
+// 6-masala
+
+
+// 7-masala
+
+
+// 8-masala
+// for(let i =1; i <= 10; i++){
+//   if(i % 2 == 0){
+//     console.log(i ** 2);
+//   } else{
+//     console.log(i ** 3);
+//   }
+// }
+
+
+// 9-masala
+// for(let i = 1; i <= 50; i++){
+//   if(i % 15 == 0){
+//     console.log(i);
+//   }
+// }
+
+
+// 10-masala
+// let res = 0
+// let sum = 0
+// let orta 
+// for(let i = 1; i <= 100; i++){
+//   res += 1
+//   sum += i
+//   orta = sum / res
+//   console.log(orta);
+// }
+
+
+// 11-masala
+
+
+// 12-masala
+
+
+// 13-masala
+
+
+// 14-masala
+// let sum = 0
+// for(let i = 2; i<= 200; i+=2){
+  // sum += i
+  // console.log(sum);
+// }
+
+
+// 15-masala
+
+
+
+// 1-masala
+// for(let i = 1; i <= 10; i++){
+//   if(i == 5){
+//     break;
+//   }
+//   console.log(i);
+// }
+
+
+// 2-masala
+// for(let i = 1; i <= 10; i++){
+//   if(i == 3){
+//     continue
+//   }
+//   if(i == 7){
+//     continue
+//   }
+//   console.log(i);
+// }
+
+
+// 3-masala
+// let num = +prompt('son kirirt')
+// if(num == 7){
+//   console.log('Topildi');
+// }
+
+
+// 4-masala
+// for(let i = 1; i <= 10; i++){
+//   if(i % 2 == 0){
+//     continue
+//   }
+//   console.log(i);
+// }
+
+
+// 5-masala
+for(let i = 1; i <= 100; i++){
+  if(i >= 50){
+    break;
+  }
+  console.log(i);
+}
+
+
+
+
+
+
